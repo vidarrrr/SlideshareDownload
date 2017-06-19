7c18b8fcd8abe86a15b9c7434543499cc9759da9
4nyp0m0n05 <kerim6017@gmail.com>
2017-06-19 11:39:55 +0300

    v2

--- slddownloader.py
+++ slddownloader.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import urllib
 for x in range(1,34):
-	urllib.urlretrieve("https://image.slidesharecdn.com/istsecsunum-170505210512/95/stsec17-kresel-siber-sula-mcadele-ve-trend-saptrma-teorisi-"+str(x)+"-638.jpg?cb=1494018394",str(x)+".jpg")
+	urllib.urlretrieve("https://image.slidesharecdn.com/xxx/95/xx-"+str(x)+"-xxx.jpg?cb=xxxxxxxx",str(x)+".jpg")
 #document.querySelector('.slide_image').src take link in your browser's console.
